4d42c5bf207f4b173af9ae1650db3fe260525758
Nick Zhukov <njhuckov@gmail.com>
2019-11-17 16:39:15 +0300

    решены задачи: * angleInRadian * thirdDigit

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -33,6 +33,7 @@ fun discriminant(a: Double, b: Double, c: Double) = sqr(b) - 4 * a * c
 fun quadraticEquationRoot(a: Double, b: Double, c: Double) =
     (-b + sqrt(discriminant(a, b, c))) / (2 * a)
 
+
 /**
  * Пример
  *
@@ -48,9 +49,19 @@ fun quadraticRootProduct(a: Double, b: Double, c: Double): Double {
 /**
  * Пример главной функции
  */
-fun main() {
-    val x1x2 = quadraticRootProduct(1.0, 13.0, 42.0)
-    println("Root product: $x1x2")
+fun main(args: Array<String>) {
+    val x = 7
+    val result = sqr(x = x)
+
+    println("$x * $x = $result")
+    //    val x1x2 = quadraticRootProduct(1.0, 13.0, 42.0)
+//    println("Root product: $x1x2")
+    val res2 = angleInRadian(36, 14, 35)
+    println("$res2")
+
+    val res3 = thirdDigit(23456)
+
+    println("$res3")
 }
 
 /**
@@ -76,7 +87,10 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double = TODO()
+fun angleInRadian(deg: Int, min: Int, sec: Int): Double =
+    (deg * PI / 180) +
+            ((min * PI) / (180 * 60)) +
+            ((sec * PI) / (180 * 60 * 60))
 
 /**
  * Тривиальная
@@ -92,7 +106,7 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = TODO()
+fun thirdDigit(number: Int): Int = (number / 100) % 10
 
 /**
  * Простая
